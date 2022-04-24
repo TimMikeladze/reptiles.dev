1724a858ef0cba20038a114c09acd8708177abd8
TimMikeladze <tim.mikeladze@gmail.com>
2022-04-23 19:50:28 -0700

    fix: 🐛 remove unused function

--- src/util/generateTiles.ts
+++ src/util/generateTiles.ts
@@ -85,18 +85,6 @@ export const toDataUrl = (svg: string): string => {
   return dataUrl;
 };
 
-export const byteLength = (str: string): number => {
-  // returns the byte length of an utf8 string
-  let s = str.length;
-  for (let i = str.length - 1; i >= 0; i--) {
-    const code = str.charCodeAt(i);
-    if (code > 0x7f && code <= 0x7ff) s++;
-    else if (code > 0x7ff && code <= 0xffff) s += 2;
-    if (code >= 0xdc00 && code <= 0xdfff) i--; //trail surrogate
-  }
-  return s;
-};
-
 export const patterns: {
   [key: string]: any;
 } = {
